de6c75b8b12c132bbde7c99b70f564a976841dd5
sirMahdiRahmani <sir.mahdirahmani@gmail.com>
2025-04-25 00:12:00 +0330

    Add Glew Library, ReLink Glfw Library And init Glew

--- OpenGL/Source/main.cpp
+++ OpenGL/Source/main.cpp
@@ -1,4 +1,7 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
+
 
 int main(void)
 {
@@ -18,6 +21,12 @@ int main(void)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+
+    if (glewInit() != GLEW_OK)
+    {
+        std::cerr << "Failed to initialize GLEW" << std::endl;
+        return -1;
+    }
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
